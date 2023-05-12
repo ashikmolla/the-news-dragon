f62539f5cd5909338989d2fcdd52c21eb947c86e
AsHiK-M(C.D) <ashik018361@gmaill.com>
2023-05-12 12:55:17 +0600

    first commit

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
+import Header from '../pages/Shared/Header/Header';
+import LeftNav from '../pages/Shared/LeftNav/LeftNav';
+import RigthNav from '../pages/Shared/RigthNav/RigthNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+
+                    <Col lg={6}>
+                    <Outlet></Outlet>
+                    </Col>
+                    <Col lg={3}>
+                        <RigthNav></RigthNav>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer></Footer>
+
+            
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftNav = () => {
+    return (
+        <div>
+            <h1>left nave</h1>
+        </div>
+    );
+};
+
+export default LeftNav;
